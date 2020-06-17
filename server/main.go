0edb35e32e70f99095b1a32efc9c4d949f69a77e
Kobatai <taiga1983346@gmail.com>
2020-06-17 22:55:07 +0900

    add: html表示機能
    htmlをtemplateとして配置しgoで処理した値を返す
    エラーがあればキャッチして表示

--- server/main.go
+++ server/main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/flosch/pongo2"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
+// templateのパス
+const tmplPath = "src/template"
+
 var e = createMux()
 
 func main() {
+	// `/`のパスとarticleIndexの処理を紐付ける
 	e.GET("/", articleIndex)
 
+	// webサーバーをポート8080で起動
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
@@ -27,6 +34,32 @@ func createMux() *echo.Echo {
 	return e
 }
 
+// httpリクエストの情報はecho.Contextの構造体に入ってくる
 func articleIndex(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+	// httpコードが200ならHello Worldの文字列をレスポンス
+	// return c.String(http.StatusOK, "Hello, World!")
+
+	data := map[string]interface{}{
+		"Message": "Hello, World!",
+		"Now":     time.Now(),
+	}
+	return render(c, "article/index.html", data)
+}
+
+func htmlBlob(file string, data map[string]interface{}) ([]byte, error) {
+	// pongo2 を利用して、テンプレートファイルとデータから HTML を生成している 返り値はbyteとそのエラー
+	return pongo2.Must(pongo2.FromCache(tmplPath + file)).ExecuteBytes(data)
+}
+
+func render(c echo.Context, file string, data map[string]interface{}) error {
+	// 定義した htmlBlob() 関数を呼び出し、生成された HTML をバイトデータとして受け取る
+	b, err := htmlBlob(file, data)
+
+	// エラーチェック
+	if err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	// ステータスコード 200 で HTML データをレスポンス
+	return c.HTMLBlob(http.StatusOK, b)
 }
