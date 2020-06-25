759f17c03fe9b8aea1b88e1ec67e683d254ae1f9
Kobatai <taiga1983346@gmail.com>
2020-06-25 20:50:11 +0900

    add:Create処理
    カラム追加した分Articleの構造体修正
    構造体のフィールドをSQLに埋め込んで実行するArticleCreate追加

--- server/model/article.go
+++ server/model/article.go
@@ -1,7 +1,12 @@
 package model
 
+import "time"
+
 //Articleの構造体にIDとdbのカラムのidをメタ情報化し、sqlxがSQLの実行結果と構造体を紐づける
 type Article struct {
-	ID    int    `db:"id"`
-	Title string `db:"title"`
+	ID      int       `db:"id"`
+	Title   string    `db:"title"`
+	Body    string    `db:"body" form:"body"`
+	Created time.Time `db:"created"`
+	Updated time.Time `db:"updated"`
 }
--- server/repository/article_repository.go
+++ server/repository/article_repository.go
@@ -2,6 +2,8 @@ package repository
 
 import (
 	"app/model"
+	"database/sql"
+	"time"
 )
 
 func ArticleList() ([]*model.Article, error) {
@@ -17,4 +19,37 @@ func ArticleList() ([]*model.Article, error) {
 	}
 
 	return articles, nil
+}
+
+func ArticleCreate(article *model.Article) (sql.Result, error) {
+
+	now := time.Now()
+
+	//  *model.Articleでポインタ参照しているのでarticle構造体のフィールドに直接入れられる
+	article.Created = now
+	article.Updated = now
+
+	query := `INSERT INTO articles (title, body, created, updated)
+	VALUES (:title, :body, :created, :updated);`
+
+	// db接続開始
+	// tx = 送信機？
+	tx := db.MustBegin()
+
+	// クエリ文字列と構造体を引数に渡して SQL を実行します。
+	// クエリ文字列内の「:title」「:body」「:created」「:updated」は構造体の値で置換
+	// 構造体タグで指定してあるフィールドが対象（`db:"title"` など）になる
+	res, err := tx.NamedExec(query, article)
+	if err != nil {
+		// 登録時エラーがあればrollback
+		tx.Rollback()
+
+		// resにnil　errにerrを返す
+		return nil, err
+	}
+
+	tx.Commit()
+
+	return res, nil
+
 }
